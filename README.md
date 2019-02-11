57ddb1f50c84b6e58e843c30df267d7b25f63e1a
Devincf <devinc.firat@gmail.com>
2019-02-11 21:28:51 +0100

    Added Keyboard Macros

--- README.md
+++ README.md
@@ -5,5 +5,13 @@ Record and playback your own mouse and keyboard macros
 ---
 
 Usage:
+To create a new Macro:  
+##python main.py rec [MACRO_NAME]
+Stop recording by pressing the ESC key!
+
+---
+
+To run a saved Macro:   
+##python main.py run [MACRO_NAME]
 
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from time import sleep
 import time
-import pyautogui
-from pynput.mouse import Listener
+from pynput.mouse import Button, Listener as MouseListener,     Controller as MouseController
+from pynput.keyboard import Key, Listener as KeyboardListener,  Controller as KeyboardController
 
 
 recordName = ""
@@ -12,13 +12,19 @@ eventlist = []
 firstTimeEvent = True
 last_timeevt = 0
 
+mouse = MouseController()
+keyboard = KeyboardController()
 
-def on_click(x,y,button,pressed):
+key_listener = 0
+mouse_listener = 0
+
+def on_mouse_click(x,y,button,pressed):
     global firstTimeEvent,last_timeevt
     
     if pressed == True:
         #print(x, " - ", y, " pressed  " , pressed)
         evt = []
+        evt.append("m")
         evt.append(x)
         evt.append(y)
         if firstTimeEvent == True:
@@ -29,24 +35,79 @@ def on_click(x,y,button,pressed):
             evt.append(time.time() - last_timeevt)
             last_timeevt = time.time()
         eventlist.append(evt)
-        print ("(",evt[0], ",", evt[1], " ,", evt[2],"),")
-
-        if len(eventlist) > 5:
-            f = open("macro.txt","a+")
-            f.write("%s=%d:%d:%d" % (recordName, eventlist[0][0],eventlist[0][1],eventlist[0][2] ) )
-            for i in range(1,len(eventlist)):
-                f.write("|%d:%d:%s" % (eventlist[i][0],eventlist[i][1],str(round(eventlist[i][2], 5)) ) )
-            f.write("\r\n")
-            f.close()
-            sys.exit()
-
     pass
+
+def finalizeMacro():
+    print("finalizing")
+    f = open("macro.txt","a+")
+    f.write("%s=" % recordName)
+    for event in eventlist:
+        print(event)
+        if event[0] == 'm':
+            f.write("m:%d:%d:%s|" %(event[1], event[2], str(round(event[3],5)) ))
+        else:
+            f.write("k:%s:%s:%s|" %(str(event[1]), event[2], str(round(event[3],2)) ))
+
+    f.write("\r\n")
+    f.close()
+
+    mouse_listener.stop()
+    key_listener.stop()
+    pass
+
+def addKey(key,press_type):
+    global firstTimeEvent,last_timeevt
+    evt = []
+    evt.append("k")
+    evt.append(key)
+    evt.append(press_type)
+    if firstTimeEvent == True:
+        firstTimeEvent = False;
+        evt.append(0)
+        last_timeevt = time.time()
+    else:
+        evt.append(time.time() - last_timeevt)
+        last_timeevt = time.time()
+        
+    eventlist.append(evt)
+    pass
+
+def on_keyboard_press(key):
+    if key == Key.esc:
+        finalizeMacro()
+    addKey(key,'0')
+
+def on_keyboard_release(key):
+    addKey(key,'1')
+
+def getKeyFromStr(key):
+    if key == "Key.enter":
+        return Key.enter
+    elif key == "Key.shift":
+        return Key.shift
+    else:
+        return key.replace("'", "")
 
 def do_event(levelList):
     for evt in levelList:
-        pyautogui.moveTo(evt[0],evt[1])
-        sleep(evt[2])
-        pyautogui.click()
+        print(evt)
+        if evt[0] == 'm':
+            if evt[3] != 0:
+                sleep(evt[3])
+            #pyautogui.moveTo(evt[1],evt[2])
+            mouse.position = (evt[1], evt[2])
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            #pyautogui.click()
+        elif evt[0] == 'k':
+            key = getKeyFromStr(evt[1])
+            if evt[3] != 0:
+                sleep(evt[3])
+            if evt[2] == '0':
+                keyboard.press(key)
+            else:
+                keyboard.release(key)
+
     pass
 
 def does_macro_exist(name):
@@ -63,22 +124,27 @@ def does_macro_exist(name):
 
 if len(sys.argv) <= 1:
     print("See usage!")
-    sys.exit()
+    quit()
 
 if sys.argv[1] == "run":
     #Execute Macro
     if len(sys.argv) <= 2:
         print("Please specify the macro to be executed")
-        sys.exit()
+        quit()
     playbackName = sys.argv[2]
     macro = does_macro_exist(playbackName)
     if macro == False:
         print("Macro:", playbackName, "doesnt exist, please see usage for more!")
-        sys.exit()
+        quit()
     macro_list = []
-    for segment in macro.split('|'):
+    macros = macro.split('|')
+    macros.pop() #remove last entry which is the newline
+    for segment in macros:
         segment_values = segment.split(':')
-        macro_list.append([int(segment_values[0]),int(segment_values[1]),float(segment_values[2])])
+        if segment_values[0] == 'm':
+            macro_list.append([segment_values[0], int(segment_values[1]),int(segment_values[2]),float(segment_values[3])])
+        else:
+            macro_list.append([segment_values[0], segment_values[1],segment_values[2], float(segment_values[3])])
     do_event(macro_list)
 
 
@@ -86,16 +152,24 @@ elif sys.argv[1] == "rec":
     #Record new Macro
     if len(sys.argv) <= 2:
         print("Please specify the macro name")
-        sys.exit()
+        quit()
     recordName = sys.argv[2]
     if does_macro_exist(recordName) != False:
         print("Macro", recordName, "already exists, please see usage for more!")
-        sys.exit()
-    with Listener(on_click=on_click) as listener:
-        listener.join()
+        quit()
+    
+    key_listener = KeyboardListener(on_press=on_keyboard_press, on_release=on_keyboard_release)
+    key_listener.start()
+
+    mouse_listener = MouseListener(on_click=on_mouse_click)
+    mouse_listener.start()
+
+    mouse_listener.join()
+    key_listener.join()
+        
 else:
     print("See usage!")
-    sys.exit()
+    quit()
 
 
 
